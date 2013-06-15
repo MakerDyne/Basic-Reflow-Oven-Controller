95388d18af36880c177691661e102fb094cf6074
Richard Leszczynski <richard@makerdyne.com>
2013-06-15 01:40:56 +0100

    Reflow profile tuned for my oven

--- Reflow_Parameters.h
+++ Reflow_Parameters.h
@@ -10,17 +10,17 @@ double MAX_STARTING_TEMP = 30;
 
 #define SOAK_TARGET_TEMP               160
 #define SOAK_TEMP_ERROR                  3
-#define SOAK_DURATION                   90
+#define SOAK_DURATION                   80
 #define SOAK_CONTROL_INTERVAL            1
 
-#define RAMP_TO_REFLOW_TARGET_TEMP     240
-#define RAMP_TO_REFLOW_TEMP_ERROR        5
-#define RAMP_TO_REFLOW_DURATION         45
+#define RAMP_TO_REFLOW_TARGET_TEMP     198
+#define RAMP_TO_REFLOW_TEMP_ERROR        1
+#define RAMP_TO_REFLOW_DURATION         30
 #define RAMP_TO_REFLOW_CONTROL_INTERVAL  1
 
-#define REFLOW_TARGET_TEMP             240
-#define REFLOW_TEMP_ERROR                2
-#define REFLOW_DURATION                 60
+#define REFLOW_TARGET_TEMP             198
+#define REFLOW_TEMP_ERROR                1
+#define REFLOW_DURATION                 30
 #define REFLOW_CONTROL_INTERVAL          1
  
 #define COOLING_TARGET_TEMP             40
